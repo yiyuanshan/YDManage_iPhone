360bee05b7230598edbe396483449b1a63bd7a4f
shanyiyuan <shanyy@lkhealth.cn>
2016-01-15 14:20:55 +0800

    添加文件

---
+++ ViewController/Find/YDFindBaseVC.m
@@ -0,0 +1,38 @@
+//
+//  YDFindBaseVC.m
+//  YDManage_iPhone
+//
+//  Created by shanyiyuan on 16/1/15.
+//  Copyright © 2016年 苏州联康网络有限公司. All rights reserved.
+//
+
+#import "YDFindBaseVC.h"
+
+@interface YDFindBaseVC ()
+
+@end
+
+@implementation YDFindBaseVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ ViewController/Info/YDInfoBaseVC.h
@@ -0,0 +1,13 @@
+//
+//  YDInfoBaseVC.h
+//  YDManage_iPhone
+//
+//  Created by shanyiyuan on 16/1/15.
+//  Copyright © 2016年 苏州联康网络有限公司. All rights reserved.
+//
+
+#import "YDBaseVC.h"
+
+@interface YDInfoBaseVC : YDBaseVC
+
+@end
